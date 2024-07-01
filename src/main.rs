65a8937b764f6b7b627f343625c3475278633657
Miquel A. Cabot <miquel.cabot@gmail.com>
2024-07-01 22:07:07 +0200

    add Block struct

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,50 @@ use std::fmt;
 use std::thread;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
+// Define difficulty of the mining process
+const DIFFICULTY: usize = 2;
+
+// Define the structure of a block in the blockchain
+struct Block {
+    index: u32,
+    previous_hash: String,
+    timestamp: u64,
+    data: String,
+    nonce: u64,
+    hash: String,
+}
+
+impl Block {
+    fn new(index: u32, previous_hash: String, data: String) -> Block {
+        let timestamp = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_secs();
+        Block {
+            index,
+            previous_hash,
+            timestamp,
+            data,
+            nonce: 0,
+            hash: String::new(),
+        }
+    }
+
+    fn calculate_hash(&mut self) -> String {
+        let data = format!(
+            "{}{}{}{}{}",
+            self.index, &self.previous_hash, self.timestamp, &self.data, self.nonce
+        );
+
+        let mut hasher = Sha256::new();
+        hasher.update(data.as_bytes());
+        let result = hasher.finalize();
+
+        let hash_str = format!("{:x}", result);
+        hash_str
+    }
+}
+
 fn main() {
     println!("Welcome to CABOTCOIN Mining Simulator!");
 }
