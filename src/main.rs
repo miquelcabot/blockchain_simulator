2b4b956f611baf5022f178b6fee6e63e95a649ac
Miquel A. Cabot <miquel.cabot@gmail.com>
2024-07-01 20:12:23 +0200

    add sha2 and chrono packages

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use sha2::{Digest, Sha256};
+use std::fmt;
+use std::thread;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to CABOTCOIN Mining Simulator!");
 }
